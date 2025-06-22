da0eb7f7df931ae6ea5d2950b597997b12b9b1ec
ZikXewen <datasup12@gmail.com>
2025-06-21 22:15:06 -0500

    threading wip

--- server/main.c
+++ server/main.c
@@ -1,45 +1,21 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #include "socket_utils.h"
 
-int main() {
-  int serverFd = createTCPIpv4Socket();
-  if (serverFd == -1) {
-    perror("fd");
-    return 1;
-  }
+#define MAX_CLIENTS 20
 
-  struct sockaddr_in serverAddr;
-  createIpv4Address(&serverAddr, "", 8000);
+struct ClientSocket {
+  int fd;
+  struct sockaddr_in addr;
+  pthread_t tid;
+};
 
-  int res = bind(serverFd, (struct sockaddr *)&serverAddr, sizeof serverAddr);
-  if (res == -1) {
-    perror("bind");
-    return -1;
-  }
-  printf("Socket bound successfully\n");
-
-  res = listen(serverFd, 10);
-  if (res == -1) {
-    perror("listen");
-    return -1;
-  }
-  printf("Listening...\n");
-
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrSz = sizeof clientAddr;
-  int clientFd =
-      accept(serverFd, (struct sockaddr *)&clientAddr, &clientAddrSz);
-  if (clientFd == -1) {
-    perror("accept");
-    return -1;
-  }
-  printf("Accepting connection...\n");
-
+void threadFn(int fd) {
   char buf[2048];
   while (1) {
-    ssize_t ct = recv(clientFd, buf, 2048, 0);
+    ssize_t ct = recv(fd, buf, 2048, 0);
     if (ct == 0) {
       break;
     }
@@ -49,9 +25,53 @@ int main() {
     }
     buf[ct] = 0;
 
-    printf("%d: %s\n", clientFd, buf);
+    printf("%d: %s\n", fd, buf);
+  }
+  shutdown(fd, SHUT_RDWR);
+  close(fd);
+}
+
+int main() {
+  int sfd = createTCPIpv4Socket();
+  if (sfd == -1) {
+    perror("fd");
+    return 1;
   }
 
-  close(clientFd);
-  close(serverFd);
+  struct sockaddr_in serverAddr;
+  createIpv4Address(&serverAddr, "", 8000);
+
+  int res = bind(sfd, (struct sockaddr *)&serverAddr, sizeof serverAddr);
+  if (res == -1) {
+    perror("bind");
+    return -1;
+  }
+  printf("Socket bound successfully\n");
+
+  res = listen(sfd, 10);
+  if (res == -1) {
+    perror("listen");
+    return -1;
+  }
+  printf("Listening...\n");
+
+  struct ClientSocket sockets[MAX_CLIENTS];
+  int accepted = 0;
+
+  while (1) {
+    socklen_t sz = sizeof(struct sockaddr_in);
+    sockets[accepted].fd =
+        accept(sfd, (struct sockaddr *)&sockets[accepted].addr, &sz);
+    if (sockets[accepted].fd == -1) {
+      perror("accept");
+      continue;
+    }
+    printf("A user joined. The group now has %d users.\n", ++accepted);
+
+    pthread_create(&sockets[accepted - 1].tid, NULL,
+                   (void *(*)(void *))threadFn,
+                   (void *)(size_t)sockets[accepted - 1].fd);
+  }
+  shutdown(sfd, SHUT_RDWR);
+  close(sfd);
 }
